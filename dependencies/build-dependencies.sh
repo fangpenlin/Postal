612ed8e4afb6cf841bc61c6547b6675b18da5ba1
Fang-Pen Lin <hello@fangpenlin.com>
2019-08-04 02:17:59 -0700

    Use latest libetpan

--- dependencies/build-dependencies.sh
+++ dependencies/build-dependencies.sh
@@ -5,8 +5,8 @@ scriptpath="`pwd`"
 popd > /dev/null
 
 # configuration
-url="https://github.com/dinhviethoa/libetpan.git"
-rev=4aee22436809af67f23170fe15106b91ff2971e6
+url="https://github.com/fangpenlin/libetpan.git"
+rev=02fe6f85729f469b587722447d0c223447e86e84
 
 ios_target="libetpan ios"
 macos_target="static libetpan"
